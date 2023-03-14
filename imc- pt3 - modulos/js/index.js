f83ec52f4b6dd51a367b8d741282727c1b437d40
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2023-03-13 21:07:44 -0300

    Delete imc- pt3 - modulos directory

--- imc- pt3 - modulos/js/index.js
+++
@@ -1,7 +0,0 @@
-import listaAlunos from "./alunos.js";
-import { exibirItems } from "./imc.js";
-import { mostrarCategorias } from "./imc.js";
-
-var lista = listaAlunos();
-lista.forEach(exibirItems);
-mostrarCategorias();
